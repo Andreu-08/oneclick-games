d496b76fdc3793dffa0be5ecce011a1d5147e10a
Andreu Orenga <andreuorenga@gmail.com>
2025-03-29 20:06:17 +0000

    feat(pruebas): creacion archivo api.php y comprobaciones para las rutas de la api

--- backend_oc/app/Http/Controllers/UserController.php
+++ backend_oc/app/Http/Controllers/UserController.php
@@ -2,16 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+   
     public function index()
     {
-        //
+        $users = User::all();
+        return response($users, 200);    
     }
 
     /**
