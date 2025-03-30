0e45603119c6d4de06caa2c29614c5c3b045112c
Andreu Orenga <andreuorenga@gmail.com>
2025-03-30 11:51:53 +0000

    feat(login): Login sencillo para creacion de usuario mediante token, rutas para login perfil y logout

--- backend_oc/routes/api.php
+++ backend_oc/routes/api.php
@@ -1,7 +1,17 @@
 <?php
 
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/users',[UserController::class, 'index']); 
+Route::get('/users',[UserController::class, 'index']);
+
+
+Route::post('/auth/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+
+    Route::get('/auth/profile', [AuthController::class, 'profile']);
+    Route::post('/auth/logout', [AuthController::class, 'logout']);
+});
