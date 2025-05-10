ede743f686e6e2de339ccfcfb177640f72aab2ed
Andreu Orenga <andreuorenga@gmail.com>
2025-05-10 07:54:32 +0000

    feat: Rutas para gestionar juegos a traves de la api y organizacion de carpetas para los controllers

--- backend_oc/routes/api.php
+++ backend_oc/routes/api.php
@@ -1,8 +1,8 @@
 <?php
 
-use App\Http\Controllers\UserController;
 use App\Http\Controllers\Api\AuthController;
-use Illuminate\Http\Request;
+use App\Http\Controllers\GameController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/users',[UserController::class, 'index']);
@@ -17,6 +17,15 @@ Route::middleware('auth:sanctum')->group(function () {
 
     Route::delete('/users/me', [UserController::class, 'destroy']);
 
+    Route::post('/games', [GameController::class, 'store'])->middleware('admin');
+    Route::put('/games/{id}', [GameController::class, 'update'])->middleware('admin');
+    Route::delete('/games/{id}', [GameController::class, 'destroy'])->middleware('admin');
+
+
 });
 
 Route::get('/users/{id}', [UserController::class, 'show']);
+
+
+Route::get('/games', [GameController::class, 'index']);
+Route::get('/games/{id}', [GameController::class, 'show']);
