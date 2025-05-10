bd21d4c91ae5aaa632b1394486a2be6af065efc8
Andreu Orenga <andreuorenga@gmail.com>
2025-05-10 08:20:48 +0000

    feat: Actualizar GameController para listar juegos activos

--- backend_oc/app/Http/Controllers/Api/GameController.php
+++ backend_oc/app/Http/Controllers/Api/GameController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Game;
 use Illuminate\Http\Request;
 
 class GameController extends Controller
 {
-    //
+
+    //index para listar todos los jeugos
+    public function index()
+    {
+        $games = Game::where('is_active', true)
+            ->select('id', 'title', 'description', 'url', 'difficulty_levels')
+            ->get();
+
+        return response()->json($games);
+    }
 }
