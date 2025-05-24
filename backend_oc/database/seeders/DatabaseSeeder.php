2a34b2819e95084aead5e6edc7168374e99069f9
Andreu Orenga <andreuorenga@gmail.com>
2025-05-24 11:04:51 +0000

    feat: agregar campo image_url a la tabla de juegos y actualizar el seeder con nuevas imágenes

--- backend_oc/database/seeders/DatabaseSeeder.php
+++ backend_oc/database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
     {
        $this->call([
             UserSeeder::class,
-            // GameSeeder::class,
+            GameSeeder::class,
             // ScoreSeeder::class,
         ]);
     }
--- backend_oc/database/seeders/GameSeeder.php
+++ backend_oc/database/seeders/GameSeeder.php
@@ -7,14 +7,12 @@ use App\Models\Game;
 
 class GameSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
         Game::create([
             'title' => 'Juego de Memoria',
             'description' => 'Encuentra las parejas lo más rápido posible',
+            'image_url' => url('/images/games/memoria.png'),
             'is_active' => true,
             'difficulty_levels' => null,
         ]);
@@ -22,8 +20,57 @@ class GameSeeder extends Seeder
         Game::create([
             'title' => 'Juego de Reflejos',
             'description' => 'Haz clic cuando veas la señal',
+            'image_url' => url('/images/games/reflejos.png'),
             'is_active' => true,
             'difficulty_levels' => ['fácil', 'difícil'],
+        ]);
+
+        Game::create([
+            'title' => 'Ordenar Palabras',
+            'description' => 'Coloca las palabras en el orden correcto',
+            'image_url' => url('/images/games/ordenar_palabras.png'),
+            'is_active' => true,
+            'difficulty_levels' => ['fácil', 'normal'],
+        ]);
+
+        Game::create([
+            'title' => 'Secuencia de Colores',
+            'description' => 'Repite la secuencia de colores que ves',
+            'image_url' => url('/images/games/secuencia_colores.png'),
+            'is_active' => true,
+            'difficulty_levels' => ['normal', 'difícil'],
+        ]);
+
+        Game::create([
+            'title' => 'Encuentra el Número',
+            'description' => 'Haz clic en el número indicado entre varios',
+            'image_url' => url('/images/games/encuentra_numero.png'),
+            'is_active' => true,
+            'difficulty_levels' => ['fácil'],
+        ]);
+
+        Game::create([
+            'title' => 'Suma Rápida',
+            'description' => 'Resuelve sumas sencillas rápidamente',
+            'image_url' => url('/images/games/suma_rapida.png'),
+            'is_active' => true,
+            'difficulty_levels' => ['fácil', 'media'],
+        ]);
+
+        Game::create([
+            'title' => 'Dibujo Invisible',
+            'description' => 'Recuerda y dibuja una figura sencilla con el teclado',
+            'image_url' => url('/images/games/dibujo_invisible.png'),
+            'is_active' => true,
+            'difficulty_levels' => null,
+        ]);
+
+        Game::create([
+            'title' => 'Selecciona el Emoji',
+            'description' => 'Selecciona el emoji correcto según la emoción',
+            'image_url' => url('/images/games/selecciona_emoji.png'),
+            'is_active' => true,
+            'difficulty_levels' => ['infantil'],
         ]);
     }
 }
