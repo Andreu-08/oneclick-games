955d34be1fc88a43a90a1c040c566930b5b7cff3
Andreu Orenga <andreuorenga@gmail.com>
2025-05-25 10:44:34 +0000

    feat: agregar nuevas imágenes de juegos y mejorar diseño en GameCard

--- frontend_oc/src/components/GameCard.vue
+++ frontend_oc/src/components/GameCard.vue
@@ -1,18 +1,18 @@
 <template>
   <article
     class="flex flex-col gap-4 p-5 rounded-2xl shadow-md w-full max-w-[400px] mx-auto hover:scale-105 hover:shadow-xl transition"
-    style="background-color: #E8D5F8"
+    style="background-color: #FEF9C2"
     tabindex="0"
     role="group"
     :aria-label="game.title"
   >
     <div class="bg-blue-100 rounded-xl px-4 py-2 text-center shadow-sm">
-      <h2 class="text-lg font-bold text-blue-900 uppercase">
+      <h2 class="text-lg font-semibold uppercase">
         {{ game.title }}
       </h2>
     </div>
 
-    <div class="rounded-xl overflow-hidden h-[180px]">
+    <div class=" rounded-xl  h-[180px] flex items-center justify-center">
       <img
         :src="game.image_url"
         :alt="`Captura del juego ${game.title}`"
@@ -21,12 +21,12 @@
     </div>
 
     <div
-      class="bg-purple-100 rounded-xl px-4 py-4 text-blue-900 text-base font-medium text-center shadow-inner h-[84px] flex items-center justify-center"
+      class="bg-purple-200 rounded-xl px-4 py-4 text-blue-900 text-base font-medium text-center shadow-inner h-[84px] flex items-center justify-center"
     >
       {{ game.description }}
     </div>
 
-    <div class="flex justify-center">
+    <div class="flex justify-center bg-green-100 rounded-xl px-4 py-4 text-blue-900 text-base font-medium text-center shadow-inner h-[84px] flex items-center justify-center">
       <router-link
         :to="`/games/${game.url}`"
         class="w-full max-w-[300px] py-3 rounded-full bg-blue-600 hover:bg-blue-700 text-white text-lg font-semibold transition text-center focus:outline-none focus:ring-4 focus:ring-blue-300"
