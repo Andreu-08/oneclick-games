8afd7fb3a1e74ad8d36a512f2a1abe1cf49b0889
Andreu Orenga <andreuorenga@gmail.com>
2025-05-27 17:08:57 +0000

    feat: actualizar API_URL para usar variable de entorno y agregar migración para el juego "Ordenar Palabras"

--- frontend_oc/src/services/auth.js
+++ frontend_oc/src/services/auth.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = 'http://localhost:8000/api'
+const API_URL = import.meta.env.VITE_API_URL
 
 // Iniciar sesión con nombre y PIN
 export const login = async (nickname, pin) => {
